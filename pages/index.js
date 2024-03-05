ff8910ac9b6b8fbaa9ad5db762d042bb3ab44f5f
Erfan <Erfansharafi60@gmail.com>
2024-03-05 11:17:12 +0330

    v2

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home({ data }) {
+  console.log(data);
+
   return (
     <div className={styles.container}>
-      <h1>app : {data}</h1>
+      <h1>page SSG with getStaticProps()</h1>
+      <ul>
+        {data.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const data = "Erfan Sharafi";
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
 
   return {
     props: {
